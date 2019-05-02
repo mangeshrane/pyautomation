917bb871ea815f5fd211fed685ae9a652320df84
Mangesh Rane <mrane>
2019-05-02 18:20:26 +0530

    updated adb class

--- tests/test_suites/test_selendroid_app.py
+++ tests/test_suites/test_selendroid_app.py
@@ -7,6 +7,7 @@ from pyautomation.mobile.mobile_test import MobileTest
 from tests.selendroid_test_app.homepage import HomePage
 from tests.selendroid_test_app.selendroid_test_app import TestApp
 import time
+from pyautomation.mobile.locator_type import UiSelector
 
 
 class TestSelendroidApp(MobileTest):
@@ -20,5 +21,8 @@ class TestSelendroidApp(MobileTest):
     def test_dismiss_alert(self):
         homepage = HomePage(self.driver)
         homepage.click_localization_btn_and_dismiss_alert()
+        el = self.driver.find_element_by_android_uiautomator(UiSelector().class_name('android.widget.Toast').locator)
+        print(el)
+        print(el.text)
         time.sleep(5)
         assert self.driver.current_activity == TestApp(self.driver).activity_id(), "Alert not dismissed"
